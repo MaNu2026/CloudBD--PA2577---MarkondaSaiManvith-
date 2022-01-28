5514be3f3365fd8f6b64e45db15a2f5c52fa8fd7
MaNu2026 <90133249+MaNu2026@users.noreply.github.com>
2022-01-28 23:27:45 +0530

    Update init.pp

--- Assignments/Cloud_assignment/puppet/modules/nginx/manifests/init.pp
+++ Assignments/Cloud_assignment/puppet/modules/nginx/manifests/init.pp
@@ -1,8 +1,9 @@
+# This code installs the current version of nginx. 
 class nginx {
   package { 'nginx':
     ensure => present;
   }
-
+# Require package should be execuated initially in order to run the service. 
   service { 'nginx':
     ensure  => running,
     require => Package['nginx'];
