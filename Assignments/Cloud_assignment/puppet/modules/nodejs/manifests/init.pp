d492c3ffa31a2e3b316654682f259953a2083007
MaNu2026 <90133249+MaNu2026@users.noreply.github.com>
2022-01-28 23:37:00 +0530

    Update init.pp

--- Assignments/Cloud_assignment/puppet/modules/nodejs/manifests/init.pp
+++ Assignments/Cloud_assignment/puppet/modules/nodejs/manifests/init.pp
@@ -1,19 +1,20 @@
+# The below installs nodejs and curl. 
 class nodejs {
-	package{ 'nodejs':
+	package{ 'nodejs':        
 			ensure => 'present',
-			require => Exec['nn']
+			require => Exec['nn']    
 	}
-
+        
 	package{ 'curl':
-			ensure => 'installed',
-			require => Exec['aptget']
+			ensure => 'installed',    # This command installs curl 
+			require => Exec['aptget'] 
 	}
 
-	exec { 'aptget':
-		command => '/usr/bin/apt-get update -y'
+	exec { 'aptget':             # This is an update command
+		command => '/usr/bin/apt-get update -y' 
 	}
 
-	exec { 'nn':
+	exec { 'nn':               # This command ensures that latest version of nodejs is installed.
 		command => '/usr/bin/curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
 		require => Exec['aptget']
 	}
