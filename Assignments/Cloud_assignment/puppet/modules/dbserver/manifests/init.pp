0a920e7ae6ad23c469fac243457d4e0b50f6ec63
MaNu2026 <90133249+MaNu2026@users.noreply.github.com>
2022-01-28 23:16:15 +0530

    Update init.pp

--- Assignments/Cloud_assignment/puppet/modules/dbserver/manifests/init.pp
+++ Assignments/Cloud_assignment/puppet/modules/dbserver/manifests/init.pp
@@ -1,8 +1,9 @@
+# The code installs the latest version of mysql-server.
 class dbserver{
-
-	package { ['mysql-server']:
+    package { ['mysql-server']:   
     ensure => latest;
   }
+  # After installing the software, this below code ensures that software is running.
     service { 'mysql':
     ensure  => running,
     require => Package['mysql-server'];
